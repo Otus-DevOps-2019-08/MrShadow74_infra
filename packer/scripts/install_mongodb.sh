a962c8a74e726e1763f453ead67e5b5b4da341b4
MrShadow74 <mrshadow74@mail.ru>
2019-09-25 09:37:29 +0000

    Add config-scripts/ and packer/, move file *.sh to config-scripts

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
+apt-get update
+apt-get install -y mongodb-org
+systemctl enable mongod
+
