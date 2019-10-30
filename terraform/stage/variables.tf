47ec5e2608af068e1b939bb3bb68ae6dc588f253
MrShadow74 <mrshadow74@mail.ru>
2019-10-30 10:04:50 +0000

    HW#10 Ansible-3

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -10,9 +10,9 @@ variable public_key_path {
   # Описание переменной
   description = "~/.ssh/appuser.pub"
 }
-variable disk_image {
-  description = "reddit-base-1569325898"
-}
+#variable disk_image {
+#  description = "reddit-base-1569325898"
+#}
 variable private_key_path {
   description = "~/.ssh/appuser"
 }
